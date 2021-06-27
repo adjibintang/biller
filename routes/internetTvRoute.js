cb550fb2e63ab477573ca88ec1712166395f90fe
darin <darinadia98@gmail.com>
2021-06-27 15:42:19 +0700

    - Add Auth Middleware To InternetTV Route

--- routes/internetTvRoute.js
+++ routes/internetTvRoute.js
@@ -1,7 +1,16 @@
 const internetTVController = require("../controller/internetTvController");
+const middleware = require("../middleware/authMiddleware");
 const router = require("express").Router();
 
-router.get("/options/:service_id", internetTVController.getInternetTVOptions);
-router.get("/information", internetTVController.getInternetAccountInfo);
+router.get(
+  "/options/:service_id",
+  middleware.userAuthorization,
+  internetTVController.getInternetTVOptions
+);
+router.get(
+  "/information",
+  middleware.userAuthorization,
+  internetTVController.getInternetAccountInfo
+);
 
 module.exports = router;

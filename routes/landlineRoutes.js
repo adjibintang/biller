8583d2e05c2611ae4539867a6bc2b134e0b067c3
anggita ruzty <ruzty.anggita@gmail.com>
2021-06-29 10:45:11 +0700

    empty commit

--- routes/landlineRoutes.js
+++ routes/landlineRoutes.js
@@ -1,5 +1,6 @@
 const landlineRouter = require('express').Router();
 const { getLandlineAccInfo } = require('../controller/landlineController');
+const middleware = require("../middleware/authMiddleware");
 
 landlineRouter.get("/info", middleware.userAuthorization, getLandlineAccInfo);
 

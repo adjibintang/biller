75e8120b92b2a781ce826e24296e967a4c95e3e9
Yasa <bintangb21@gmail.com>
2021-07-12 17:08:37 +0800

    Update BPJS API
    Create Get Period API, Update Get BPJS Customer Info API, Update New BPJS Bill, Update BPJS Model

--- controller/bpjsController.js
+++ controller/bpjsController.js
@@ -1,18 +1,27 @@
 const bpjsService = require("../service/bpjsService");
 
+exports.getPeriod = async (req, res) => {
+  try {
+    const periodResult = await bpjsService.getPeriod();
+
+    return res.status(200).json({
+      statusText: "Ok",
+      message: "Success Get Period",
+      data: periodResult,
+    });
+  } catch (error) {
+    return res.sendStatus(500);
+  }
+};
+
 exports.getBpjsCustomerInfo = async (req, res) => {
   try {
     const customerInfoResult = await bpjsService.getCustomerInfo(
-      req.body.customerNumber
+      req.body.customerNumber,
+      req.body.month ? req.body.month : 1
     );
 
     if (customerInfoResult === null) return res.sendStatus(204);
-    if (customerInfoResult === 202)
-      return res.status(202).json({
-        statusText: "Accepted",
-        message:
-          "Your Account is Disabled, Please Contact The Office For Further Information",
-      });
 
     return res.status(200).json({
       statusText: "Ok",
@@ -20,7 +29,6 @@ exports.getBpjsCustomerInfo = async (req, res) => {
       data: customerInfoResult,
     });
   } catch (error) {
-    console.log(error);
     return res.sendStatus(500);
   }
 };
@@ -32,20 +40,12 @@ exports.createNewBill = async (req, res) => {
       req.user.id
     );
 
-    if (createNewBillResult === 204) return res.sendStatus(204);
-    if (createNewBillResult === 202)
-      return res.status(202).json({
-        statusText: "Accepted",
-        message:
-          "Your Account is Disabled, Please Contact The Office For Further Information",
-      });
-
     return res.status(200).json({
       statusText: "Ok",
       message: "Create New Bill",
       data: createNewBillResult,
     });
   } catch (error) {
-    return res.sendStatus(5001);
+    return res.sendStatus(500);
   }
 };
--- database/seeders/20210701060517-seed-bank-transfer.js
+++ database/seeders/20210701060517-seed-bank-transfer.js
@@ -2,16 +2,32 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    // await queryInterface.bulkInsert(
-    //   "People",
-    //   [
-    //     {
-    //       name: "John Doe",
-    //       isBetaMember: false,
-    //     },
-    //   ],
-    //   {}
-    // );
+    await queryInterface.bulkInsert(
+      "bank_transfers",
+      [
+        {
+          transaction_payment_id: 1,
+          bank_destination_id: 1,
+          account_name: null,
+          account_number: null,
+          account_bank: null,
+          receipt_url: null,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          transaction_payment_id: 2,
+          bank_destination_id: 1,
+          account_name: null,
+          account_number: null,
+          account_bank: null,
+          receipt_url: null,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
   down: async (queryInterface, Sequelize) => {

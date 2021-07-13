b52a5ffb903b2cb37899ce5499bd7a34e7f19763
anggita ruzty <ruzty.anggita@gmail.com>
2021-07-13 21:37:27 +0700

    edit seeders & migration file

--- database/seeders/20210626032635-seed-electricities.js
+++ database/seeders/20210626032635-seed-electricities.js
@@ -1,4 +1,4 @@
-// const faker = require("faker");
+const faker = require("faker");
 ("use strict");
 
 module.exports = {
--- database/seeders/20210701060357-seed-pln-tagihan-bill.js
+++ database/seeders/20210701060357-seed-pln-tagihan-bill.js
@@ -1,51 +1,20 @@
-const faker = require("faker");
 "use strict";
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert(
-      "pln_tagihan_bills",
-      [
-        {
-          bill_id: 1,
-          customer_number: faker.datatype.number(),
-          name: faker.name.findName(),
-          rates: "R1",
-          power: "900V",
-          tagihan_date: new Date(),
-          last_month_stand_meter: 1804,
-          this_month_stand_meter: 2054,
-          bill_fee: 350000,
-          admin_fee: 3000,
-          late_payment_fee: 0,
-          total: 353000,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          
-        },
-
-        {
-          bill_id: 2,
-          customer_number: faker.datatype.number(),
-          name: faker.name.findName(),
-          rates: "R1",
-          power: "1000V",
-          tagihan_date: new Date(),
-          last_month_stand_meter: 1990,
-          this_month_stand_meter: 2100,
-          bill_fee: 480000,
-          admin_fee: 3000,
-          late_payment_fee: 0,
-          total: 483000,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      ],
-      {}
-    );
+    // await queryInterface.bulkInsert(
+    //   "People",
+    //   [
+    //     {
+    //       name: "John Doe",
+    //       isBetaMember: false,
+    //     },
+    //   ],
+    //   {}
+    // );
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete("pln_tagihan_bills", null, {});
+    //await queryInterface.bulkDelete("People", null, {});
   },
 };

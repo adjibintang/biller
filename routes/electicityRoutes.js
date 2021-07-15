82d9b02282f6670cb21ed0f3453f3ce3bc8af135
anggita ruzty <ruzty.anggita@gmail.com>
2021-07-15 15:10:01 +0700

    edit router

--- routes/electicityRoutes.js
+++ routes/electicityRoutes.js
@@ -2,10 +2,10 @@ const electricityRouter = require('express').Router();
 const { getTagihanAccInfo, getElectricityOptions, getTokenPricelist, getTokenAccInfo, postTagihanBill, postTokenBill } = require('../controller/electricityController');
 const middleware = require("../middleware/authMiddleware");
 
-electricityRouter.get("/tagihan/info", middleware.userAuthorization, getTagihanAccInfo);
+electricityRouter.post("/tagihan/info", middleware.userAuthorization, getTagihanAccInfo);
 electricityRouter.get("/options/:service_id", middleware.userAuthorization, getElectricityOptions);
-electricityRouter.get("/token/blank", middleware.userAuthorization, getTokenPricelist);
-electricityRouter.get("/token/info", middleware.userAuthorization, getTokenAccInfo);
+electricityRouter.post("/token/blank", middleware.userAuthorization, getTokenPricelist);
+electricityRouter.post("/token/info", middleware.userAuthorization, getTokenAccInfo);
 electricityRouter.post("/tagihan/bankpayment", middleware.userAuthorization, postTagihanBill);
 electricityRouter.post("/token/bankpayment", middleware.userAuthorization, postTokenBill);
 
--- routes/landlineRoutes.js
+++ routes/landlineRoutes.js
@@ -2,7 +2,7 @@ const landlineRouter = require('express').Router();
 const { getLandlineAccInfo, postLandlineBill } = require('../controller/landlineController');
 const middleware = require("../middleware/authMiddleware");
 
-landlineRouter.get("/info", middleware.userAuthorization, getLandlineAccInfo);
+landlineRouter.post("/info", middleware.userAuthorization, getLandlineAccInfo);
 landlineRouter.post("/bankpayment", middleware.userAuthorization, postLandlineBill);
 
 module.exports = landlineRouter;

df1633e5b348b1c256891cc92d8efd58734d1c0a
anggita ruzty <ruzty.anggita@gmail.com>
2021-07-20 00:27:36 +0700

    notification api

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const bpjsRoute = require("./routes/bpjsRoute");
 const paymentRoute = require("./routes/paymentRoute");
 const receiptRoute = require("./routes/receiptRoute");
 const internetTVRoute = require("./routes/internetTVRoute");
+const notificationRoute = require("./routes/notificationRoute");
 
 server.use(logger("dev"));
 server.use(cors());
@@ -40,6 +41,7 @@ server.use("/api/biller/payment", paymentRoute);
 server.use("/api/biller/receipt", receiptRoute);
 server.use("/api/biller/internet_TV", internetTVRoute);
 server.use("/api/biller/mobile/bill", mobileRoute);
+server.use("/api/biller/notification", notificationRoute);
 
 server.all("*", (req, res) => {
   res.status(404).json({
